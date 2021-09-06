46ed80d9c39ac4d8d2fc7b44b49dd7c8eef59dc0
Your Name <you@example.com>
2021-09-06 11:20:41 +0300

    HW8

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-// import { lazy, Suspense } from "react";
+import { lazy, Suspense } from 'react';
 
 import Container from './components/Container';
 import AppBar from './components/AppBar';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { authOperations } from './redux/auth';
-// import { contactsOperations } from './redux';
-// import { connect } from 'react-redux';
-import HomeView from './views/HomeView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
-import ContactsView from './views/ContactsView';
 
-//   const HomeView = lazy(() =>
-//   import('./views/HomeView'/* webpackChunkName: "HomeView"*/)
-// );
-// const RegisterView = lazy(() =>
-//   import('./views/RegisterView' /* webpackChunkName: "RegisterView" */)
-// );
-// const LoginView = lazy(() =>
-//   import('./views/LoginView' /* webpackChunkName: "LoginView" */)
-// );
-// const ContactsView = lazy(() =>
-//   import('./views/ContactsView' /* webpackChunkName: "ContactsView" */)
-// );
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+
+const HomeView = lazy(() =>
+  import('./views/HomeView' /* webpackChunkName: "HomeView"*/),
+);
+const RegisterView = lazy(() =>
+  import('./views/RegisterView' /* webpackChunkName: "RegisterView" */),
+);
+const LoginView = lazy(() =>
+  import('./views/LoginView' /* webpackChunkName: "LoginView" */),
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "ContactsView" */),
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -38,26 +35,25 @@ function App() {
     <>
       <Container>
         <AppBar />
-
-        {/* <Suspense fallback={<div>Loading...</div>}> */}
         <Switch>
-          <Route path="/" exact>
-            <HomeView />
-          </Route>
+          <Suspense fallback={<div>Loading...</div>}>
+            <PublicRoute exact path="/">
+              <HomeView />
+            </PublicRoute>
 
-          <Route path="/register">
-            <RegisterView />
-          </Route>
+            <PublicRoute path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
 
-          <Route path="/login">
-            <LoginView />
-          </Route>
+            <PublicRoute path="/login" redirecTo="/contacts" restricted>
+              <LoginView />
+            </PublicRoute>
 
-          <Route path="/contacts">
-            <ContactsView />
-          </Route>
+            <PrivateRoute path="/contacts">
+              <ContactsView />
+            </PrivateRoute>
+          </Suspense>
         </Switch>
-        {/* </Suspense> */}
       </Container>
     </>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
+import { authSelectors } from '../../redux/auth';
 
-const Navigation = () => (
-  <nav>
-    <NavLink
-      to="/"
-      exact
-      className={styles.Navigationlink}
-      activeClassName={styles.NavigationActiveLink}
-    >
-      HOME
-    </NavLink>
-
-    <NavLink
-      to="/contacts"
-      exact
-      className={styles.Navigationlink}
-      activeClassName={styles.NavigationActiveLink}
-    >
-      CONTACTS
-    </NavLink>
-  </nav>
-);
+const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        exact
+        className={styles.Navigationlink}
+        activeClassName={styles.NavigationActiveLink}
+      >
+        HOME
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          exact
+          className={styles.Navigationlink}
+          activeClassName={styles.NavigationActiveLink}
+        >
+          CONTACTS
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
